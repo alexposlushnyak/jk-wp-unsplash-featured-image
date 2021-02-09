361b72cbf5d326d1c66ee6815c259f8f3a277c4f
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-09 21:50:30 +0200

    Added panel render method (tmp)

--- jk-wp-unsplash-featured-image.php
+++ jk-wp-unsplash-featured-image.php
@@ -2,13 +2,62 @@
 
 $path = __DIR__;
 
-final class jk_wp_unsplash_featured_image
+class jk_wp_unsplash_featured_image
 {
 
-    public function init()
+    public function panel_render()
     {
 
+        $post_id = $_POST['post_id'];
 
+        ?>
+
+        <div class="components-panel__body unsplash-featured-image-panel">
+
+            <h2 class="components-panel__body-title">
+
+                <button type="button" aria-expanded="false" class="components-button components-panel__body-toggle">
+
+                    <span aria-hidden="true">
+
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                             class="components-panel__arrow" role="img" aria-hidden="true" focusable="false">
+
+                            <path d="M17.5 11.6L12 16l-5.5-4.4.9-1.2L12 14l4.5-3.6 1 1.2z"></path>
+
+                        </svg>
+
+                    </span>
+
+                    <?php echo esc_html('Unsplash Featured Image'); ?>
+
+                </button>
+
+            </h2>
+
+            <div class="unsplash-modal-button-wrapper">
+
+                <div class="unsplash-image-placeholder"
+                    <?php if (has_post_thumbnail($post_id)): ?>
+                        style="background-image: url(<?php echo esc_url(get_the_post_thumbnail_url($post_id)); ?>)"<?php endif; ?>>
+
+                </div>
+
+                <button class="unsplash-modal-button">
+
+                    <?php echo esc_html('Find Image'); ?>
+
+                    <i class="fab fa-unsplash"></i>
+
+                </button>
+
+            </div>
+
+        </div>
+
+        <?php
+
+        die();
 
     }
 
